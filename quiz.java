0aeb1cbe5888a5bf97e53b54ee8224cd86d46892
haneesh <haneeshramineni@gmail.com>
2022-05-16 19:58:11 +0530

    quizprogram

---
+++ quiz.java
@@ -0,0 +1,534 @@
+import java.util.Scanner;
+class A
+{
+	A()
+	{	
+            System.out.println("welcome to c exam..here is your first question in C.. ");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("1Q)Who is the fater of c language \n 1)Steve Jobs \n 2)James Gosling \n 3)Dennis Ritchie \n 4)Rasmus Lerdof ");
+	
+		System.out.print("ENter the Option : ");
+		int A=sc.nextInt();
+		switch(A)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            int count=0;
+            if(A==3){
+                        count++;
+                   
+                     }
+            System.out.println("2Q)Which of the following is not a valid C variable name? \n 1)int number; \n 2)float rate; \n 3)int variable_count; \n 4)int $main ");
+	
+		System.out.print("ENter the Option : ");
+		int B=sc.nextInt();
+		switch(B)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(B==4){
+                        count++;
+                   
+                     }
+            System.out.println("3Q)All keywords in C are------- \n 1)Lowercase letters \n 2)Uppercase letters \n 3)Camelcase letters \n 4)None of te mentioned ");
+	
+		System.out.print("ENter the Option : ");
+		int C=sc.nextInt();
+		switch(C)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(C==1){
+                        count++;
+                   
+                     }
+            System.out.println("4Q)Which is valid C expression? \n 1)int my_num=100,000; \n 2)int my_num=100000; \n 3)int my num=1000; \n 4)int $my_num=10000; ");
+	
+		System.out.print("ENter the Option : ");
+		int D=sc.nextInt();
+		switch(D)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(D==2){
+                        count++;
+                   
+                     }
+            System.out.println("5Q)Which of te following cannot be a variable in C? \n 1)volatile \n 2)true \n 3)friend \n 4)export ");
+	
+		System.out.print("ENter the Option : ");
+		int E=sc.nextInt();
+		switch(E)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(E==1){
+                        count++;
+                   
+                     }
+            System.out.println("6Q)What is short int in C programming \n 1)The basic data type in C \n 2)Qualifier \n 3)none \n 4)Both 1&2 ");
+	
+		System.out.print("ENter the Option : ");
+		int F=sc.nextInt();
+		switch(F)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(F==3){
+                        count++;
+                   
+                     }
+            System.out.println("7Q)Which keyword is used to prevent any changes in the variable witin a c program? \n 1)immutable \n 2)mutable \n 3)const \n 4)volatile ");
+	
+		System.out.print("ENter the Option : ");
+		int G=sc.nextInt();
+		switch(G)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(G==3){
+                        count++;
+                   
+                     }
+            System.out.println("8Q)Which of the following type cast is accepted by C language? \n 1)Widening conversions \n 2)Narrowing Conversions \n 3)Bot 1&2 \n 4)None of the above ");
+	
+		System.out.print("ENter the Option : ");
+		int H=sc.nextInt();
+		switch(H)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(H==3){
+                        count++;
+                   
+                     }
+            System.out.println("9Q)What is an example of iteration in C? \n 1)for \n 2)while \n 3)do-while \n 4)all of the above ");
+	
+		System.out.print("ENter the Option : ");
+		int I=sc.nextInt();
+		switch(I)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(I==4){
+                        count++;
+                   
+                     }
+            System.out.println("10Q)Functions in C language are always------ \n 1)Internal \n 2)External \n 3)Both 1&2 \n 4)None ");
+	
+		System.out.print("ENter the Option : ");
+		int ch=sc.nextInt();
+		switch(ch)
+		{
+			
+			default :
+				System.out.println("Answered all questions");
+				break;
+		}
+           
+            if(ch==2){
+                        count++;
+                     }
+          
+            if(count>=7){
+                         System.out.println("your score is:" +count);
+                         System.out.println("you are eligible for cpp exam");
+                         System.out.println("----------------------------------\n----------------------------");
+                         System.out.println("Here is your first question in cpp.Good Luck..");
+                         System.out.println("1Q)Who invented c++ \n 1)Dennis Ritchie \n 2)Ken Thompson \n 3)Brian Kernighan \n 4)Bjarne Stroustrup ");
+	
+		System.out.print("ENter the Option : ");
+		int AA=sc.nextInt();
+		switch(AA)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            int total=0;
+            if(AA==4){
+                        total++;
+                   
+                     }
+            System.out.println("2Q)Which of the following is used for comments in cpp \n 1)/*comment*/ \n 2)//comment*/ \n 3)//comment \n 4)Both 3&1 ");
+	
+		System.out.print("ENter the Option : ");
+		int BB=sc.nextInt();
+		switch(BB)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(BB==4){
+                        total++;
+                   
+                     }
+            System.out.println("3Q)Which of the following user-defined header file extension used in cpp\n 1)hg \n 2)cpp \n 3)h \n 4)hf ");
+	
+		System.out.print("ENter the Option : ");
+		int CC=sc.nextInt();
+		switch(CC)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(CC==3){
+                        total++;
+                   
+                     }
+            System.out.println("4Q)Which of the following is a correct identifire in cpp \n 1)VAR_1234 \n 2)$var_name \n 3)7VARNAME \n 4)7var_name ");
+	
+		System.out.print("ENter the Option : ");
+		int DD=sc.nextInt();
+		switch(DD)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(DD==1){
+                        total++;
+                   
+                     }
+            System.out.println("5Q)Which of the following approach is used by cpp \n 1)1 left-right \n 2)right-left \n 3)Bottom-up \n 4)Top-down ");
+	
+		System.out.print("ENter the Option : ");
+		int EE=sc.nextInt();
+		switch(EE)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(EE==3){
+                        total++;
+                   
+                     }
+            System.out.println("6Q)By default all the files in cpp are opened in------mode. \n 1)Binary \n 2)VTC \n 3)Text \n 4)ISCII ");
+	
+		System.out.print("ENter the Option : ");
+		int FF=sc.nextInt();
+		switch(FF)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(FF==3){
+                        total++;
+                   
+                     }
+            System.out.println("7Q)Which of the following correctly declares an array in cpp? \n 1)array{10}; \n 2)array array[10] \n 3)int array; \n 4)int array[10] ");
+	
+		System.out.print("ENter the Option : ");
+		int GG=sc.nextInt();
+		switch(GG)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(GG==4){
+                        total++;
+                   
+                     }
+            System.out.println("8Q)Which is more effective while calling te cpp functions? \n 1)call by object \n 2)call by pointer \n 3)call by value \n 4)call by reference ");
+	
+		System.out.print("ENter the Option : ");
+		int HH=sc.nextInt();
+		switch(HH)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(HH==4){
+                        total++;
+                   
+                     }
+            System.out.println("9Q)Which of the following is used to terminate the function declaration in cpp 1); \n 2)] \n 3)) \n 4): ");
+	
+		System.out.print("ENter the Option : ");
+		int II=sc.nextInt();
+		switch(II)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(II==1){
+                        total++;
+                   
+                     }
+            System.out.println("10Q)Which keyboard is used to define the macros in cpp \n 1)#macro \n 2)#define \n 3)macro \n 4)define ");
+	
+		System.out.print("ENter the Option : ");
+		int chh=sc.nextInt();
+		switch(chh)
+		{
+			
+			default :
+				System.out.println("Answered all questions");
+				break;
+		}
+           
+            if(chh==2){
+                        total++;
+                     }
+          
+            if(total>=7){
+                         System.out.println("your score is:" +total);
+                         System.out.println("you are eligible for java exam");
+                         System.out.println("----------------------------------\n----------------------------");
+                         System.out.println("Here is your first question in java.Good Luck..");
+                         System.out.println("1Q)Size of byte in java \n 1)1 byte \n 2)2 \n 3)3 \n 4)4 ");
+	
+		System.out.print("ENter the Option : ");
+		int AAA=sc.nextInt();
+		switch(AAA)
+		{
+		
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            int sum=0;
+            if(AAA==1){
+                        sum++;
+                   
+                     }
+            System.out.println("2Q)Number of primitive data types in java are? \n 1)6 \n 2)7 \n 3)8 \n 4)9 ");
+	
+		System.out.print("ENter the Option : ");
+		int BBB=sc.nextInt();
+		switch(BBB)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(BBB==3){
+                        sum++;
+                   
+                     }
+            System.out.println("3Q)What is the size of float and double in java? \n 1)32 and 64 \n 2)32 and 32 \n 3)64 and 64 \n 4)64 and 32 ");
+	
+		System.out.print("ENter the Option : ");
+		int CCC=sc.nextInt();
+		switch(CCC)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(CCC==1){
+                       sum++;
+                   
+                     }
+            System.out.println("4Q)Automatic type conversion is possible in which of te possible cases? \n 1)Byte to int \n 2)int to long \n 3)Long to int \n 4)Short to int ");
+	
+		System.out.print("ENter the Option : ");
+		int DDD=sc.nextInt();
+		switch(DDD)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(DDD==2){
+                        sum++;
+                   
+                     }
+            System.out.println("5Q)Select the valid statement \n 1)char[]ch=new char(5) \n 2)char[]ch=new char[5] \n 3)char[]ch=new char() \n 4)char[]ch=new char[] ");
+	
+		System.out.print("ENter the Option : ");
+		int EEE=sc.nextInt();
+		switch(EEE)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(EEE==2){
+                        sum++;
+                   
+                     }
+            System.out.println("6Q)When an array is passed to a method,what does the method receive?\n 1)The reference of the array \n 2)A copy of the array \n 3)Length of the array \n 4)Copy of first element ");
+	
+		System.out.print("ENter the Option : ");
+		int FFF=sc.nextInt();
+		switch(FFF)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(FFF==1){
+                        sum++;
+                   
+                     }
+            System.out.println("7Q)Select the valid statement to declare and initialize an array \n 1)int[]A={} \n 2)int[]A={1,2,3} \n 3)int[]A=(1,2,3) \n 4)int[][]A={1,2,3} ");
+	
+		System.out.print("ENter the Option : ");
+		int GGG=sc.nextInt();
+		switch(GGG)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(GGG==2){
+                        sum++;
+                   
+                     }
+            System.out.println("8Q)Array in java are- \n 1)Object references \n 2)objects \n 3)Primitive data types \n 4)None ");
+	
+		System.out.print("ENter the Option : ");
+		int HHH=sc.nextInt();
+		switch(HHH)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				
+		}
+            
+            if(HHH==2){
+                        sum++;
+                   
+                     }
+            System.out.println("9Q)When is the object created with new keyword \n 1)At run time \n 2)At compile time \n 3)Depends on the code \n 4)None ");
+	
+		System.out.print("ENter the Option : ");
+		int III=sc.nextInt();
+		switch(III)
+		{
+			
+			default :
+				System.out.println("Answered.Proceed to the next question ");
+				break;
+		}
+            
+            if(III==1){
+                        sum++;
+                   
+                     }
+            System.out.println("10Q)CompareTo() returns \n 1)True \n 2)False \n 3)An int value \n 4)None ");
+	
+		System.out.print("ENter the Option : ");
+		int cch=sc.nextInt();
+		switch(cch)
+		{
+			
+			default :
+				System.out.println("Answered all questions");
+				break;
+		}
+           
+            if(cch==3){
+                        sum++;
+                     }
+          
+            if(sum>=7){
+                         System.out.println("your score is:" +sum);
+                       }
+            else{
+                 System.out.println("you failed the exam try retaking the exam");
+                 System.out.println("your score is " +sum);
+               }}
+            else{
+                 System.out.println("you failed the exam try retaking the exam");
+                 System.out.println("your score is " +total);
+               }
+                       }
+            else{
+                 System.out.println("you failed the exam try retaking the exam");
+                 System.out.println("your score is " +count);
+               }
+        
+	}
+}
+class Demo7
+{
+	public static void main(String args[])
+	{
+            
+          
+		A a =new A();	
+	}
+}             
+
+
+
+
+
+
+
+
+
